f3c8bbddd79c12d4056f223d18ed36aa062d8cac
Shan Bhaseen <32348723+sbhaseen@users.noreply.github.com>
2021-03-30 15:55:15 -0400

    Updated api uri

--- src/api.js
+++ src/api.js
@@ -1,26 +1,25 @@
-// export const resourceURL = 'http://localhost:5000/'; // Local development
-export const resourceURL = 'https://cryptic-sands-86857.herokuapp.com/';
+export const resourceURL = "http://localhost:5000/"; // Local development
 
 export default {
   // api/items
   // GET all (+ page=X&limit=Y)
-  getAllItems: resourceURL + 'api/items?',
+  getAllItems: resourceURL + "api/items?",
   // GET one (+ :id)
-  getOneItem: resourceURL + 'api/items/',
+  getOneItem: resourceURL + "api/items/",
   // POST
-  createItem: resourceURL + 'api/items/',
+  createItem: resourceURL + "api/items/",
   // UPDATE (+ :id)
-  updateItem: resourceURL + 'api/items/',
+  updateItem: resourceURL + "api/items/",
   // DELETE (+ :id)
-  deleteItem: resourceURL + 'api/items/delete/',
+  deleteItem: resourceURL + "api/items/delete/",
 
   // api/auth
   // POST
-  sendAuth: resourceURL + 'api/auth',
+  sendAuth: resourceURL + "api/auth",
   // GET
-  getUser: resourceURL + 'api/auth/user',
+  getUser: resourceURL + "api/auth/user",
 
   // api/users
   // POST
-  createUser: resourceURL + 'api/users'
+  createUser: resourceURL + "api/users",
 };
