45b575c06526ee905d3b5b68f16fcc67b55e7938
Shan Bhaseen <32348723+sbhaseen@users.noreply.github.com>
2021-08-05 15:50:24 -0400

    Update server dependencies

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const app = require('./app');
-const mongoose = require('mongoose');
+const app = require("./app");
+const mongoose = require("mongoose");
 
 // DB Connection
 const db = process.env.MONGODB_URI || process.env.DEV_DB;
+
+console.log(`const db = ${db}`);
 
 // Connect to MongoDB instance
 mongoose
@@ -14,10 +16,10 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
-  .then(() => console.log('MongoDB connected successfully.'))
-  .catch(err => console.log('MongoDB connection error: ' + err));
+  .then(() => console.log("MongoDB connected successfully."))
+  .catch((err) => console.log("MongoDB connection error: " + err));
 
 const port = process.env.PORT || 5000;
 
